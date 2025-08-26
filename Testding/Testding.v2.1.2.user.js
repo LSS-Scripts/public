6d1b9db9afd0b597b81c90b19fa3823386a8b500
Masklin112 <jl@112.io>
2025-08-26 17:18:15 +0200

    Create Testding.v2.1.2.user.js

---
+++ Testding/Testding.v2.1.2.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Testding
+// @namespace    https://github.com/LSS-Scripts/public
+// @version      2.1.2
+// @description  Just a test... 
+// @author       Dein Name (und Gemini)
+// @match        https://www.leitstellenspiel.de/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
